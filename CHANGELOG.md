d7bf538e883c5a8c8e9ebc24de7c238571ed7bc1
wetblanketcc <35851045+wetblanketcc@users.noreply.github.com>
2018-07-13 10:34:23 -0700

    Version bump.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+#### 1.0.2 2018/07/13
+
+* Fixing keepalive issue
+* Added debug statement to help detect miner/proxy misconfiguration
+
 #### 1.0.1 2018/07/12
 
 * Adding in worker socket keepalive configuration
--- aiostratum_proxy/__init__.py
+++ aiostratum_proxy/__init__.py
@@ -4,6 +4,6 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 __appname__ = __package__.replace('_', '-')
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 app_version = "{}/{}".format(__appname__, __version__)
